6b2384c6531482dde9c4aca6cc4f82e12178e66a
Matthew Keil <malibuss67@gmail.com>
2020-07-08 16:32:29 -0400

    roughed out custom-post-deploy plugin

--- .serverless_plugins/custom-post-deploy.ts
+++ .serverless_plugins/custom-post-deploy.ts
@@ -1,10 +1,30 @@
+import { DynamoDB, Lambda } from "aws-sdk";
 import Serverless from "serverless";
 import Plugin from "serverless/classes/Plugin";
 
-interface CustomPostDeployOptions {}
-
 export class CustomPostDeploy implements Plugin {
-  hooks = {};
+  postDeploy = async () => {
+    const { cli, service, getProvider } = this.serverless;
+    const aws = getProvider("aws");
+    const region = aws.getRegion();
+    const FunctionName = service
+      .getAllFunctions()
+      .find(name => name.endsWith("post-deploy-handler"));
+    const { Payload } = await new Lambda({ region }).invoke({ FunctionName }).promise();
+    const { Item } = await new DynamoDB({ region })
+      .getItem({
+        // @ts-ignore the typscript definition is incorrect and resources is not defined
+        TableName: service.resources.Resources.Table.Properties.TableName,
+        Key: { id: { "S": Payload.toString() } }
+      })
+      .promise();
+    const item = JSON.stringify(DynamoDB.Converter.unmarshall(Item));
+    cli.log(`successfully got the following item out of DynamoDB: ${item}`);
+  };
 
-  constructor(public serverless: Serverless, options: Serverless.Options) {}
+  hooks = {
+    "after:deploy:finalize": this.postDeploy
+  };
+
+  constructor(private serverless: Serverless, private options: Serverless.Options) {}
 }
